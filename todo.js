0d6ecea3b1462c9fd4d2d157f7867a4fbec07666
kaylayshi <kelechi@lotusbetaanalytics.com>
2017-07-19 17:40:14 +0100

    i have it done

---
+++ todo.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+
+	app.controller('Todoctrl', function($scope) {
+		$scope.todos = [];
+
+		$scope.addTodo = function(){
+			var newTodo={
+				done: false,
+				text: $scope.todoText,
+				priority: $scope.todoPriority
+			};
+
+			$scope.todos.push(newTodo);
+			$scope.todoText = '';
+			$scope.todoPriority = 'normal';
+		};
+
+		$scope.getTotal = function(){
+			return $scope.todos.length; 
+		};
+
+		$scope.removeTodo = function(start){
+			$scope.todos.splice(start, 1);
+		};
+
+		$scope.move = function(index, direction){
+			if(direction === 'up'){
+				if(index === 0){
+					return;
+				}
+				index = index -1;
+			}
+			if (direction === 'down'){
+				if(index === $scope.todos.length -1){
+					return;
+				}
+			}
+			var todo = $scope.todos[index];
+			$scope.todos.spliice(index+2, 0, todo);
+			$scope.todos.splice(index, 1);
+		};
+	});
