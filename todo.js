94e96404d74fb30771e6a84387209c6214168daa
kaylayshi <kelechi@lotusbetaanalytics.com>
2017-07-19 18:16:01 +0100

    fair enough

--- todo.js
+++ todo.js
@@ -1,43 +1,18 @@
-(function () {
-    var app = angular.module('Todo-app', []);
-
-	app.controller('Todoctrl', function($scope) {
-		$scope.todos = [];
-
-		$scope.addTodo = function(){
-			var newTodo={
-				done: false,
-				text: $scope.todoText,
-				priority: $scope.todoPriority
-			};
-
-			$scope.todos.push(newTodo);
-			$scope.todoText = '';
-			$scope.todoPriority = 'normal';
-		};
-
-		$scope.getTotal = function(){
-			return $scope.todos.length; 
-		};
-
-		$scope.removeTodo = function(start){
-			$scope.todos.splice(start, 1);
-		};
-
-		$scope.move = function(index, direction){
-			if(direction === 'up'){
-				if(index === 0){
-					return;
-				}
-				index = index -1;
-			}
-			if (direction === 'down'){
-				if(index === $scope.todos.length -1){
-					return;
-				}
-			}
-			var todo = $scope.todos[index];
-			$scope.todos.spliice(index+2, 0, todo);
-			$scope.todos.splice(index, 1);
-		};
-	});
+angular.module('ToDo',[]).
+			controller('todoController', ['$scope',function($scope){
+				$scope.todos = [
+						{'title':'Build a todo app','done':false}
+					];
+					$scope.addTodo = function(){
+						$scope.todos.push({'title':$scope.newTodo,'done':false})
+						$scope.newTodo = ''
+					}
+					$scope.clearCompleted =function(){
+						$scope.todos= $scope.todos.filter(function(item){
+							return !item.done
+						})
+					};
+					$scope.getTotal = function(){
+						return $scope.todos.length;
+					};
+			}])
